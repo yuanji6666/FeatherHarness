2c167edf1182e86698cabd29ff49ab4b1c5a9160
袁霁 <2419408174@qq.com>
2026-04-14 15:51:33 +0800

    use postgres as short memory storage

--- agent/lead_agent.py
+++ agent/lead_agent.py
@@ -1,20 +1,18 @@
 from langchain.agents import create_agent
-from langgraph.checkpoint.memory import MemorySaver
 
 from middleware.summarization import get_summarization_middleware
 from model.factory import create_chat_model
 from tools import GetToolRegistery
 
 from .prompt import get_system_prompt
-def create_lead_agent():
-    memory = MemorySaver()
-    leader = create_agent(
-        model = create_chat_model(),
-        tools = [tool for tool in GetToolRegistery(enable_task_tools=True).values()],     
-        checkpointer=memory,
-        system_prompt=get_system_prompt(),
-        middleware=[get_summarization_middleware()]
-    )
-    
-    return leader
+async def create_lead_agent(memory):
+        leader = create_agent(
+            model = create_chat_model(),
+            tools = [tool for tool in GetToolRegistery(enable_task_tools=True).values()],     
+            checkpointer=memory,
+            system_prompt=get_system_prompt(),
+            middleware=[get_summarization_middleware()]
+        )
+        
+        return leader
 
--- client.py
+++ client.py
@@ -2,31 +2,35 @@ from agent import create_lead_agent
 from langchain.agents import AgentState
 from langchain.messages import HumanMessage
 from dotenv import load_dotenv
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
 
+import os
 import asyncio
 
 load_dotenv()
 
-leader = create_lead_agent()
 
 agent_state = AgentState(messages=[])
 
 
 async def main():
-    while True:
-        query = input("\n请输入您的问题：")
-        if query.lower() in ["/bye"]:
-            print("exit!")
-            break
-        agent_state['messages'].append(HumanMessage(content=query))
+    async with AsyncPostgresSaver.from_conn_string(os.getenv("DATABASE_URL")) as memory: 
+        await memory.setup()
+        leader = await create_lead_agent(memory) 
+        while True:
+            query = input("\n请输入您的问题：")
+            if query.lower() in ["/bye"]:
+                print("exit!")
+                break
+            agent_state['messages'].append(HumanMessage(content=query))
 
-        async for chunk in leader.astream(
-            input=agent_state, 
-            config={"configurable":{'thread_id':1}}, 
-            stream_mode='messages', 
-            version='v2'
-        ):
-            print(chunk['data'][0].content, end='', flush=True)
+            async for chunk in leader.astream(
+                input=agent_state, 
+                config={"configurable":{'thread_id':1}}, 
+                stream_mode='messages', 
+                version='v2'
+            ):
+                print(chunk['data'][0].content, end='', flush=True)
 
 
 
